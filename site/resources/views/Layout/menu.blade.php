d4e192b91119f135d3e52bcaf650b854304f3c9f
Md. Jannat UL Naim <72838150+Naim317@users.noreply.github.com>
2021-08-19 00:31:48 +0600

    Last update
    Last update

--- site/resources/views/Layout/menu.blade.php
+++ site/resources/views/Layout/menu.blade.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-3 mt-lg-0">
             <li class="nav-item active">
-                <a class="nav-link nav-font" href="#">Home </a>
+                <a class="nav-link nav-font" href="{{url('/')}}">Home </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link nav-font" href="{{url('/Course')}}">Courses</a>
